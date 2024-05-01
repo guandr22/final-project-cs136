305b81bfc272166333d0542f9dde4c6378e11ca1
Wyatt <wyatt@MacBook-Air-237.local>
2024-05-01 17:39:20 -0400

    adding the interface

--- mapnyc/QuadtreeInterface.java
+++ mapnyc/QuadtreeInterface.java
@@ -1,1 +1,31 @@
 package mapnyc;
+
+public interface Quadtree<Item, Location>{
+
+	// Returns true if the Quadtree is empty.  
+	public abstract boolean isEmpty();
+
+	// Returns the number of non-empty nodes.
+	public abstract int size();
+
+	// Adds an object to the tree given a place to put it.
+	// Rebalances the quadtree to ensure that no node has more than 4 children.
+	// Returns true if the addition is successful and false if the target location 
+	// is already occupied.
+	// If successful, increases the Quadtree's size.
+	public abstract boolean insert(Item object, Location place);
+
+	// Removes an object from the tree. Returns true if the removal is successful
+	// and false if the object was not present in the tree.
+	// If successful, decreases the Quadtree's size.
+	public abstract boolean remove(Item object);
+
+	// Returns the object at a given location if one exists. Returns null otherwise/
+	public abstract Item get(Location place);
+
+	// Returns the closest object to a given location. 
+	public abstract Item closestObject(Location place);
+
+	// Returns an ArrayList of all objects within a given distance from a location.
+	public abstract ArrayList<Item> withinDistance(Location place, float radius); 
+} 
